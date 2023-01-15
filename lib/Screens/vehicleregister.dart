b4ee3f4e44145f2ea52e16a933feb23d5498f6f1
dilshanbeligala <beligaladilshan@gmail.com>
2023-01-15 21:21:15 +0530

    connect vehicle register to backend

--- lib/Screens/vehicleregister.dart
+++ lib/Screens/vehicleregister.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:second/Screens/login.dart';
 
 import 'package:second/Screens/vehicleownerdashboard.dart';
-//import 'package:http/http.dart' as http;
-//import 'package:untitled/screens/user.dart';
-
-//import 'home.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class VehicleRegister extends StatefulWidget {
   const VehicleRegister({Key? key}) : super(key: key);
@@ -57,14 +56,49 @@ class MyCustomForm extends StatefulWidget {
 
 class _MyCustomFormState extends State<MyCustomForm> {
   final _formKey = GlobalKey<FormState>();
+  final _vname = TextEditingController();
+  final _vehiclenumber = TextEditingController();
+  final _vehicletype = TextEditingController();
+  final _nic = TextEditingController();
+  final _vphonenumber = TextEditingController();
+  final _vemail = TextEditingController();
+  final _vpassword = TextEditingController();
+  final _vrepassword = TextEditingController();
 
-  final items = [
-    'Item 1',
-    'Item 1',
-    'Item 1',
-    'Item 1',
-    'Item 1',
-  ];
+  authenticate(
+    _vname,
+    _vemail,
+    _vphonenumber,
+    _nic,
+    _vehiclenumber,
+    _vehicletype,
+    _vpassword,
+  ) async {
+    var response = await http.post(
+      Uri.parse(
+          'https://fuelapp-backend-production.up.railway.app/api/v1/register_vehicleowner'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        "name": _vname.text.toString(),
+        "email": _vemail.text.toString(),
+        "phone_number": _vphonenumber.text.toString(),
+        "nic": _nic.text.toString(),
+        "vehicle_number": _vehiclenumber.text.toString(),
+        "vehicle_type": _vehicletype.text.toString(),
+        "password": _vpassword.text.toString(),
+        "role": "VEHICLEOWNER"
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      print('Hooreeyyy!!');
+      Navigator.push(context, MaterialPageRoute(builder: (_) => Login()));
+    } else {
+      print(response.body.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,20 +108,21 @@ class _MyCustomFormState extends State<MyCustomForm> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.fromLTRB(60, 10, 25, 10),
+              padding: const EdgeInsets.fromLTRB(40, 10, 25, 10),
               child: TextFormField(
+                controller: _vname,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter your name';
                   }
                   return null;
                 },
-                decoration: InputDecoration(
-                  fillColor: Color.fromARGB(255, 220, 219, 219),
-                  filled: true,
-                  border: OutlineInputBorder(
+                decoration: const InputDecoration(
+                  // fillColor: Color.fromARGB(255, 220, 219, 219),
+                  // filled: true,
+                  border: UnderlineInputBorder(
                     borderSide: BorderSide(
-                        color: Colors.white,
+                        color: Color.fromARGB(255, 8, 8, 8),
                         width: 2.0,
                         style: BorderStyle.solid),
                     borderRadius: BorderRadius.all(Radius.circular(10)),
@@ -98,9 +133,10 @@ class _MyCustomFormState extends State<MyCustomForm> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(60, 10, 25, 10),
+              padding: const EdgeInsets.fromLTRB(40, 10, 25, 10),
               //padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
               child: TextFormField(
+                controller: _vehiclenumber,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter your Vehicle number';
@@ -108,9 +144,9 @@ class _MyCustomFormState extends State<MyCustomForm> {
                   return null;
                 },
                 decoration: const InputDecoration(
-                  fillColor: Color.fromARGB(255, 220, 219, 219),
-                  filled: true,
-                  border: OutlineInputBorder(
+                  // fillColor: Color.fromARGB(255, 220, 219, 219),
+                  // filled: true,
+                  border: UnderlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                   ),
                   hintText: 'Vehicle number',
@@ -119,8 +155,31 @@ class _MyCustomFormState extends State<MyCustomForm> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(60, 10, 25, 10),
+              padding: const EdgeInsets.fromLTRB(40, 10, 25, 10),
               child: TextFormField(
+                controller: _vehicletype,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your Vehicle Type';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  // fillColor: Color.fromARGB(255, 220, 219, 219),
+                  // filled: true,
+                  border: UnderlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  hintText:
+                      'Vehicle Type (MOTORBIKE , THREEWHEEL , VAN , LORY , BUS , CAR)',
+                  hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(40, 10, 25, 10),
+              child: TextFormField(
+                controller: _nic,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter your NIC';
@@ -128,9 +187,9 @@ class _MyCustomFormState extends State<MyCustomForm> {
                   return null;
                 },
                 decoration: const InputDecoration(
-                  fillColor: Color.fromARGB(255, 220, 219, 219),
-                  filled: true,
-                  border: OutlineInputBorder(
+                  // fillColor: Color.fromARGB(255, 220, 219, 219),
+                  // filled: true,
+                  border: UnderlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                   ),
                   hintText: 'NIC',
@@ -139,9 +198,10 @@ class _MyCustomFormState extends State<MyCustomForm> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(60, 10, 25, 10),
+              padding: const EdgeInsets.fromLTRB(40, 10, 25, 10),
               //padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
               child: TextFormField(
+                controller: _vphonenumber,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter your phone number';
@@ -149,9 +209,9 @@ class _MyCustomFormState extends State<MyCustomForm> {
                   return null;
                 },
                 decoration: const InputDecoration(
-                  fillColor: Color.fromARGB(255, 220, 219, 219),
-                  filled: true,
-                  border: OutlineInputBorder(
+                  // fillColor: Color.fromARGB(255, 220, 219, 219),
+                  // filled: true,
+                  border: UnderlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                   ),
                   hintText: 'Phone number',
@@ -160,8 +220,9 @@ class _MyCustomFormState extends State<MyCustomForm> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(60, 10, 25, 10),
+              padding: const EdgeInsets.fromLTRB(40, 10, 25, 10),
               child: TextFormField(
+                controller: _vemail,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter your e-mail';
@@ -174,9 +235,9 @@ class _MyCustomFormState extends State<MyCustomForm> {
                   }
                 },
                 decoration: const InputDecoration(
-                  fillColor: Color.fromARGB(255, 220, 219, 219),
-                  filled: true,
-                  border: OutlineInputBorder(
+                  // fillColor: Color.fromARGB(255, 220, 219, 219),
+                  // filled: true,
+                  border: UnderlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                   ),
                   hintText: 'E-mail',
@@ -185,9 +246,10 @@ class _MyCustomFormState extends State<MyCustomForm> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(60, 10, 25, 10),
+              padding: const EdgeInsets.fromLTRB(40, 10, 25, 10),
               //padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
               child: TextFormField(
+                controller: _vpassword,
                 obscureText: true,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -196,9 +258,9 @@ class _MyCustomFormState extends State<MyCustomForm> {
                   return null;
                 },
                 decoration: const InputDecoration(
-                  fillColor: Color.fromARGB(255, 220, 219, 219),
-                  filled: true,
-                  border: OutlineInputBorder(
+                  // fillColor: Color.fromARGB(255, 220, 219, 219),
+                  // filled: true,
+                  border: UnderlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                   ),
                   hintText: 'Password',
@@ -207,9 +269,10 @@ class _MyCustomFormState extends State<MyCustomForm> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(60, 10, 25, 10),
+              padding: const EdgeInsets.fromLTRB(40, 10, 25, 10),
               //padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
               child: TextFormField(
+                controller: _vrepassword,
                 obscureText: true,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -218,9 +281,9 @@ class _MyCustomFormState extends State<MyCustomForm> {
                   return null;
                 },
                 decoration: const InputDecoration(
-                  fillColor: Color.fromARGB(255, 220, 219, 219),
-                  filled: true,
-                  border: OutlineInputBorder(
+                  // fillColor: Color.fromARGB(255, 220, 219, 219),
+                  // filled: true,
+                  border: UnderlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                   ),
                   hintText: 'Confirm Password',
@@ -241,14 +304,20 @@ class _MyCustomFormState extends State<MyCustomForm> {
                       color: Color.fromARGB(255, 114, 9, 1),
                       borderRadius: BorderRadius.circular(20)),
                   child: TextButton(
-                    onPressed: () {
+                    //
+                    onPressed: (() {
                       if (_formKey.currentState!.validate()) {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (_) => VehicleOwnerDashBoard()));
+                        authenticate(
+                          _vname,
+                          _vemail,
+                          _vphonenumber,
+                          _nic,
+                          _vehiclenumber,
+                          _vehicletype,
+                          _vpassword,
+                        );
                       }
-                    },
+                    }),
                     child: Text(
                       'Sign Up',
                       style: TextStyle(
